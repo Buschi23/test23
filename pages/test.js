c7e88662b461c7fbbd5d1e8d9405fa470de052e7
Buschi23 <107368011+Buschi23@users.noreply.github.com>
2022-06-20 13:43:47 +0200

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -8,7 +8,6 @@ const Home = (props) => {
         <Head>
          <meta charset="UTF-8">
          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-         <link rel="stylesheet" href="style.css">
         </Head>
         
     
