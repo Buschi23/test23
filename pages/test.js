0dfa49132a6e82accbdf36d69801e3e95f738385
Buschi23 <107368011+Buschi23@users.noreply.github.com>
2022-06-20 13:41:20 +0200

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -6,10 +6,86 @@ const Home = (props) => {
     <>
       <div className="home-container">
         <Head>
-          <title>Copy of Landing Page1</title>
-          <meta property="og:title" content="Copy of Landing Page1" />
+         <meta charset="UTF-8">
+         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         <link rel="stylesheet" href="style.css">
         </Head>
         
+    
+    
+    
+ <body>
+    
+    <div class="top-banner">
+        <p class="current-plan"> Your current Plan</p>
+        <p class="plan-type"> Starter Trial . 500MAUs</p>
+    </div>
+
+    <div class="container">
+      <h1 class="title">Choose a plan</h1>
+      <div class="toggle-switch">
+            <span>Billed anually </span>
+            <input type="checkbox" class="toggler">
+            <span> Billed monthly</span>
+      </div>
+
+      <div class="cards">
+
+        <!-- Starter Plan -->
+          <div class="card" id="card-1">
+            <h1 class="card-title">Starter</h1>
+            <h2 class="card-price" id="starter-price"> $19 <span> / month</span> </h2>
+            <ul class="card-plan">
+                <li> 500MAUs</li>
+                <li> 3 projects</li>
+                <li>Unlimted guides</li>
+                <li>Unlimted flows</li>
+                <li>Unlimted branded thems</li>
+                <li>Email Support</li>
+            </ul>
+            <button type="button" class="card-btn"> Choose Plan</button>
+          </div>
+
+          <!-- Pro Plan -->
+          <div class="card active" id="card-2">
+            <h1 class="card-title">Pro</h1>
+            <h2 class="card-price" id="pro-price"> $99 <span> / month</span> </h2>
+          
+            <select  id="maus">
+                <option value="500">500 MAUS</option>
+                <option value="100">1000 MAUS</option>
+                <option value="1500">1500 MAUS</option>
+                <option value="2000">2000 MAUS</option>
+                <option value="2500">2500 MAUS</option>
+            </select>
+            <span class="note"> Monthely active users </span>
+          
+            <ul class="card-plan">
+                <li> All starter featured ,Plus : </li>
+                <li> Unlimted projects</li>
+                <li>Unlimted fully customizable themes</li>
+                <li>A dedicated customer Success Manager</li>
+            </ul>
+            <button type="button" class="card-btn"> Choose Plan</button>
+          </div>
+
+          <!-- Enterpise Card -->
+          <div class="card" id="card-3">
+            <h1 class="card-title">Enterprise</h1>
+            <h2 class="card-price"> Let's Talk! </h2>
+            <ul class="card-plan">
+                <li> All pro featured</li>
+                <li> Unlimted MAUs</li>
+                <li> Detected enviroment</li>
+                <li> Enterprise account administration</li>
+                <li> Premium account and services</li>
+            </ul>
+            <button type="button" class="card-btn"> Contact Us</button>
+          </div>
+      </div>
+    </div>
+    <script src="main.js"></script>
+</body>       
     
     
     
@@ -18,198 +94,253 @@ const Home = (props) => {
       </div>
       <style jsx>
         {`
-          .home-container {
-            width: 100%;
-            height: auto;
-            display: flex;
-            min-height: 100vh;
-            align-items: center;
-            flex-direction: column;
-          }
-          .home-navbar-container {
-            top: 0;
-            width: 100%;
-            display: flex;
-            z-index: 100;
-            position: sticky;
-            box-shadow: 5px 5px 10px 0px #b9b9b9;
-            align-items: center;
-            flex-direction: column;
-            justify-content: center;
-            background-color: var(--dl-color-gray-white);
-          }
-          .home-navbar {
-            width: 100%;
-            display: flex;
-            max-width: var(--dl-size-size-maxcontent);
-            align-items: center;
-            padding-top: var(--dl-space-space-doubleunit);
-            padding-left: var(--dl-space-space-tripleunit);
-            padding-right: var(--dl-space-space-tripleunit);
-            flex-direction: row;
-            padding-bottom: 12px;
-            justify-content: space-between;
-          }
-          .home-left-side {
-            display: flex;
-            align-items: center;
-            flex-direction: row;
-            justify-content: center;
-          }
-          .home-image {
-            width: 50px;
-            align-self: center;
-            object-fit: cover;
-            margin-right: var(--dl-space-space-doubleunit);
-          }
-          .home-burger-menu {
-            display: none;
-            align-items: flex-start;
-            flex-direction: column;
-          }
-          .home-icon {
-            width: 36px;
-            height: 36px;
-          }
-          .home-links-container {
-            display: flex;
-            align-items: flex-start;
-            flex-direction: row;
-          }
-          .home-link {
-            margin-right: var(--dl-space-space-tripleunit);
-            text-decoration: none;
-          }
-          .home-link1 {
-            margin-right: var(--dl-space-space-tripleunit);
-            text-decoration: none;
-          }
-          .home-link2 {
-            margin-right: var(--dl-space-space-tripleunit);
-            text-decoration: none;
-          }
-          .home-link3 {
-            text-decoration: none;
-          }
-          .home-right-side {
-            display: flex;
-            align-items: center;
-            flex-direction: row;
-            justify-content: space-between;
-          }
-          .home-cta-btn {
-            color: var(--dl-color-gray-white);
-            padding-top: var(--dl-space-space-unit);
-            padding-left: var(--dl-space-space-fiveunits);
-            border-radius: var(--dl-radius-radius-radius24);
-            padding-right: var(--dl-space-space-fiveunits);
-            padding-bottom: var(--dl-space-space-unit);
-            text-decoration: none;
-            background-color: var(--dl-color-gray-black);
-          }
-          .home-mobile-menu {
-            top: 0px;
-            flex: 0 0 auto;
-            left: 0px;
-            width: 100%;
-            height: 100vh;
-            display: none;
-            padding: var(--dl-space-space-doubleunit);
-            position: absolute;
-            align-items: flex-start;
-            flex-direction: column;
-            background-color: #fff;
-          }
-          .home-container1 {
-            flex: 0 0 auto;
-            width: 100%;
-            display: flex;
-            align-items: center;
-            margin-bottom: var(--dl-space-space-doubleunit);
-            flex-direction: row;
-            justify-content: space-between;
-          }
-          .home-image1 {
-            width: 50px;
-            align-self: center;
-            object-fit: cover;
-            margin-right: var(--dl-space-space-doubleunit);
-          }
-          .home-close-menu {
-            flex: 0 0 auto;
-            display: flex;
-            align-items: flex-start;
-            flex-direction: column;
-          }
-          .home-icon2 {
-            width: 24px;
-            height: 24px;
-          }
-          .home-links-container1 {
-            display: flex;
-            align-items: flex-start;
-            flex-direction: column;
-          }
-          .home-link4 {
-            margin-bottom: var(--dl-space-space-unit);
-            text-decoration: none;
-          }
-          .home-link5 {
-            margin-bottom: var(--dl-space-space-unit);
-            text-decoration: none;
-          }
-          .home-link6 {
-            margin-bottom: var(--dl-space-space-unit);
-            text-decoration: none;
-          }
-          .home-link7 {
-            text-decoration: none;
-          }
-          .home-features {
-            width: 100%;
-            height: 1046px;
-            display: flex;
-            align-self: center;
-            align-items: center;
-            padding-top: var(--dl-space-space-tenunits);
-            padding-left: var(--dl-space-space-tripleunit);
-            flex-direction: column;
-            padding-bottom: var(--dl-space-space-fiveunits);
-            justify-content: flex-start;
-            background-image: linear-gradient(
-              to bottom,
-              #ffffff 0%,
-              #c8e9f9 70%
-            );
-          }
-          .home-text {
-            height: 0px;
-          }
-          @media (max-width: 767px) {
-            .home-burger-menu {
-              display: flex;
-              padding: var(--dl-space-space-halfunit);
-              border-radius: var(--dl-radius-radius-radius4);
-              background-color: var(--dl-color-gray-black);
-            }
-            .home-icon {
-              fill: var(--dl-color-gray-white);
-            }
-            .home-links-container {
-              display: none;
-            }
-          }
-          @media (max-width: 479px) {
-            .home-cta-btn {
-              font-size: 16px;
-              padding-left: var(--dl-space-space-doubleunit);
-              padding-right: var(--dl-space-space-doubleunit);
-            }
-            .home-features {
-              padding-top: var(--dl-space-space-fiveunits);
-              padding-bottom: var(--dl-space-space-fiveunits);
-            }
-          }
+           
+          * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+*:focus {
+  outline: none;
+}
+
+ul {
+  list-style: none;
+}
+
+
+body{
+  font-family: Arial, Helvetica, sans-serif;
+  background-color: #191b1d;
+  color: white;
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.top-banner {
+  background-color: #bc1e4a;
+  position: absolute;
+  padding: 1.5rem 4rem;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  text-align: center;
+  border-radius: 0 0 10px 10px;
+}
+
+p.current-plan {
+  font-size: 0.75rem;
+  margin-bottom: 0.5rem;
+}
+
+p.plan-type {
+  font-size: 1rem;
+  font-weight: 900;
+}
+
+.container {
+  text-align: center;
+  width: 1100px;
+  height: 60%;
+}
+
+h1.title {
+  letter-spacing: 2px;
+  margin-bottom: 1.5rem;
+}
+
+.toggle-switch{
+    display: flex;
+    height: 30px;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 1.5rem;
+}
+
+.toggler{
+    appearance: none;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    width: 50px;
+    height: 25px;
+    background-color: #bc1e4a;
+    border-radius: 25px;
+    margin: 0 1rem;
+    position: relative;
+}
+
+.toggler::before{
+    content: "";
+    width: 16px;
+    height: 16px;
+    background-color: white;
+    position: absolute;
+    border-radius: 50%;
+    top:50%;
+    transform: translateY(-50%);
+    right:4px;
+}
+
+.toggler:checked:before{
+    right: 55%;
+}
+
+.cards{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    flex-wrap: wrap;
+    height: 100%;
+}
+
+.card{
+   height: 500px;
+   width: 32%;
+   background-color: #22262C; 
+   border-radius: 15px;
+   padding: 2rem;
+   text-align: center;
+}
+
+.card-title{
+    font-size: 1.5rem;
+    font-weight: lighter;
+    margin-bottom: 1rem;
+}
+
+.card-price{
+    font-size: 3rem;
+    text-align: center;
+}
+
+h2>span{
+    font-size: 1rem;
+}
+
+#card-3 .card-price{
+    font-size: 2rem;
+} 
+
+#card-1 .card-price{
+    margin-bottom: 4rem;
+}
+.card ul{
+    text-align: left;
+    font-size: 0.9rem;
+    font-weight: lighter;
+}
+
+.card ul li:before{
+    content:"•";
+    margin-right: 0.5rem;
+    color:#bc1e4a;
+    font-size: 1.5rem;
+}
+
+.card ul li{
+    margin-bottom: 0.5rem;
+}
+
+.card-btn{
+    margin-top: 1rem;
+    width: 100%;
+    background-color: transparent;
+    border: 1px solid #bc1e4a;
+    color:white;
+    font-size: 1rem;
+    padding: 1rem;
+    border-radius: 5px;
+}
+
+#card-1 .card-btn:hover, 
+#card-3 .card-btn:hover, 
+.active .card-btn
+{
+    background-color: #bc1e4a;
+}
+
+#maus{
+  background: transparent;
+  color:white;
+  border-color: #545c5c;
+  margin-top: 1rem;
+  width: 100%;
+  padding: 1rem 2rem;
+  font-size: 1rem;
+  border-radius: 5px;
+}
+
+option{
+    font-size: 1.2rem;
+    background-color: #333;
+}
+
+.note{
+    color:#bc1e4a;
+    font-size: 0.75rem;
+    text-align: left;
+    display: block;
+    margin-top: 5px;
+}
+
+#card-2 ul{
+    margin-top: 3rem;
+}
+
+#card-3 ul{
+    margin-top: 7.5rem;
+}
+
+@media (max-width:1112px){
+    .cards{
+        justify-content: center;
+    }
+
+    .card{
+        width: 40%;
+    margin: 2rem;
+    }
+}
+
+@media (max-width:848px){
+    .card{
+        width:50%
+    }
+}
+
+@media (max-width:674px){
+    .top-banner{
+        width: 70%;
+    }
+    .card{
+        width: 70%;
+    }
+}
+
+@media (max-width:500px){
+    .top-banner{
+        width: 90%;
+    }
+
+
+
+    .card{
+        width: 90%;
+    }
+}
+          
+          
+          
+          
+          
+          
+          
         `}
       </style>
     </>
